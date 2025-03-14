441e28911c9c48dc9d189d5d25409f21fc968277
Prathamesh Kulkarni <kulkarni@cytocybernetics.com>
2025-03-14 11:47:03 -0400

    Exercise 3.5 solution.

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const app = express()
 
 let data = [
     { 
-      "id": "1", 
+      "id": 1, 
       "name": "Arto Hellas", 
       "number": "040-123456"
     },
     { 
-      "id": "2",
+      "id": 2,
       "name": "Ada Lovelace", 
       "number": "39-44-5323523"
     },
     { 
-      "id": "3",
+      "id": 3,
       "name": "Dan Abramov", 
       "number": "12-43-234345"
     },
     { 
-      "id": "4",
+      "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
 ]
+
+app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
   return response.json(data)
@@ -33,7 +35,7 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+  const id = parseInt(request.params.id)
   const person = data.find(person => person.id === id)
   if (!person) {
     return response.status(404).send('Person not found')
@@ -42,13 +44,24 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+  const id = parseInt(request.params.id)
   const index = data.findIndex(person => person.id === id)
   if (index === -1) {
     return response.status(404).send('Person not found')
   }
   data.splice(index, 1)
-  return response.json(data)
+  return response.status(200).send(`Person with id ${id} deleted`)
+})
+
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
+  if (!name || !number) {
+    return response.status(400).send('Name and number are required')
+  }
+  const id = Math.floor(Math.random() * 1000000) + 1
+  const newPerson = { "id": id, "name": name, "number": number }
+  data.push(newPerson)
+  return response.status(201).json(newPerson)
 })
 
 const PORT = 3001
