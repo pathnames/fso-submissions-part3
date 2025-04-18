4a712b40e265ca40f050ea0cf79d49772eb4eac9
pathnames <pk58@buffalo.edu>
2025-04-18 16:41:19 -0400

    Exercise 3.19 solution.

--- src/models/phonebook.js
+++ src/models/phonebook.js
@@ -14,7 +14,11 @@ mongoose
     })
 
 const phonebookEntrySchema = new mongoose.Schema({
-    name: String, 
+    name: {
+        type: String, 
+        minLength: 3, 
+        required: true
+    }, 
     number: String
 })
 
